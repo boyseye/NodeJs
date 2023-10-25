67db230f2018ae5a47742452b194ee51452ae036
GritAndGrit <boyseye@naver.com>
2023-10-26 05:19:54 +0900

    init

---
+++ controllers/page.js
@@ -0,0 +1,14 @@
+exports.renderProfile = (req,res,next) => {
+    res.render('profile', { title: '내정보 - NodeBird '});
+};
+
+exports.renderJoin = (req,res,next) => {
+    res.render('join',{title:'회원가입 - NodeBird'});
+};
+
+exports.renderMain = (req,res,next) => {
+    res.render('main',{
+        title: 'NodeBird',
+        twits: [],
+    });
+};
